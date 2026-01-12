c6298832d6807ad794fb5d9159616fe6b4a8d296
David <david.czaplinski@ymail.com>
2026-01-12 02:01:57 +0100

    Fix orphan type export

--- packages/js/core/src/index.ts
+++ packages/js/core/src/index.ts
@@ -1,4 +1,4 @@
 export type { IslandsMap, IslandConfig, StreamHandle } from "./types";
 export { createHooks, type CreateHooksOptions } from "./createHooks";
 export { useStream, type UseStreamOptions } from "./useStream";
-export { useForm, type FormProps, type UseFormOptions, type FieldProps } from "./useForm";
+export { useForm, type FormProps, type FieldProps } from "./useForm";
